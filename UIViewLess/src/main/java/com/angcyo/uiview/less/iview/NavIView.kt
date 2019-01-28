4c574e12d5515f874a95f1d732fd338dcf67e9ef
angcyo <angcyo@126.com>
2019-01-28 14:37:47 +0800

    add naviview

---
+++ UIViewLess/src/main/java/com/angcyo/uiview/less/iview/NavIView.kt
@@ -0,0 +1,82 @@
+package com.angcyo.uiview.less.iview
+
+import android.view.View
+import android.view.ViewGroup
+import com.angcyo.uiview.less.kotlin.childs
+
+/**
+ * Created by angcyo on 2018/12/02 17:08
+ * 导航栏控制
+ */
+
+class NavIView(val viewGroup: ViewGroup) {
+
+    companion object {
+        fun get(viewGroup: ViewGroup): NavIView {
+            return NavIView(viewGroup)
+        }
+    }
+
+    //默认选中第一个
+    var currentPosition = 0
+
+    var onNavSelector: OnNavSelector? = null
+
+    private val navViews = mutableListOf<View>()
+
+    /**
+     * 执行配置
+     * */
+    fun doIt() {
+
+        val size = viewGroup.childCount
+        viewGroup.childs { index, childView ->
+            navViews.add(childView)
+            if (onNavSelector?.onCanSelector(index) == true) {
+                childView.setOnClickListener {
+                    selector(index)
+                }
+            }
+        }
+
+        if (size > 0) {
+            viewGroup.post {
+                onNavSelector?.onNavSelector(-1, currentPosition)
+            }
+        }
+    }
+
+    /**
+     * 选中某一项
+     * */
+    fun selector(index: Int) {
+        val size = viewGroup.childCount
+
+        if (index < size && onNavSelector?.onCanSelector(index) == true) {
+            val itemView = navViews[index]
+            if (itemView.isSelected) {
+                //重复选择
+                onNavSelector?.onNavReSelector(index)
+            } else {
+                val oldPosition = currentPosition
+                if (size > currentPosition) {
+                    navViews[currentPosition].isSelected = false
+                }
+                itemView.isSelected = true
+                currentPosition = index
+
+                onNavSelector?.onNavSelector(oldPosition, index)
+            }
+        }
+    }
+
+    interface OnNavSelector {
+        fun onNavSelector(fromPosition: Int, toPosition: Int)
+        fun onNavReSelector(position: Int) {
+
+        }
+
+        fun onCanSelector(position: Int): Boolean = true
+
+    }
+}
