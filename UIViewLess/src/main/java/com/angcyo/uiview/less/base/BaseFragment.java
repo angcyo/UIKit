50e1e78ec9e7a029f458df597430b7457f24a4e8
angcyo <angcyo@126.com>
2019-02-26 18:13:54 +0800

    * 界面多级嵌套管理的BUG

--- UIViewLess/src/main/java/com/angcyo/uiview/less/base/BaseFragment.java
+++ UIViewLess/src/main/java/com/angcyo/uiview/less/base/BaseFragment.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -88,10 +89,15 @@ public abstract class BaseFragment extends AbsLifeCycleFragment {
 
     @Nullable
     public FragmentManager parentFragmentManager() {
-        if (getParentFragment() == null) {
+        Fragment parentFragment = getParentFragment();
+        if (parentFragment == null) {
             return getFragmentManager();
         } else {
-            return getParentFragment().getFragmentManager();
+            if (parentFragment instanceof BaseFragment) {
+                return ((BaseFragment) parentFragment).parentFragmentManager();
+            } else {
+                return parentFragment.getFragmentManager();
+            }
         }
     }
 
