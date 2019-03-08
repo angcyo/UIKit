d499cae93047acc3d049adc02f57e36f21f3ba7a
angcyo <angcyo@126.com>
2019-03-08 16:26:43 +0800

    * 优化 服务器 地址选择

--- UIViewLess/src/main/java/com/angcyo/uiview/less/base/dialog/HttpConfigDialog.java
+++ UIViewLess/src/main/java/com/angcyo/uiview/less/base/dialog/HttpConfigDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import com.angcyo.http.Http;
 import com.angcyo.http.Json;
@@ -13,8 +14,13 @@ import com.angcyo.uiview.less.R;
 import com.angcyo.uiview.less.recycler.RBaseViewHolder;
 import com.angcyo.uiview.less.utils.RDialog;
 import com.angcyo.uiview.less.utils.T_;
+import com.angcyo.uiview.less.widget.OnSpinnerItemSelected;
+import com.angcyo.uiview.less.widget.RSpinner;
+import com.angcyo.uiview.less.widget.RSpinnerAdapter;
 import retrofit2.RetrofitServiceMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,7 +32,14 @@ import java.util.Map;
 public class HttpConfigDialog {
     public static String MAPPING_URL = "https://www.angcyo.com/api/php/android/c/url_mapping";
 
-    public static void show(@NonNull Context context, final String baseUrl, @Nullable final OnHttpConfig onHttpConfig) {
+    public static void show(@NonNull final Context context, final String baseUrl, @Nullable final OnHttpConfig onHttpConfig) {
+        show(context, baseUrl, null, onHttpConfig);
+    }
+
+    /**
+     * @param urlList 可以使用空格, key:value 的形式, 会取空格分隔后的最后一个
+     */
+    public static void show(@NonNull final Context context, final String baseUrl, final List<String> urlList, @Nullable final OnHttpConfig onHttpConfig) {
         RDialog.build(context)
                 .setCanceledOnTouchOutside(false)
                 .setContentLayoutId(R.layout.base_http_config_layout)
@@ -62,6 +75,37 @@ public class HttpConfigDialog {
                                         });
                                     }
                                 });
+                            }
+                        });
+
+                        RSpinner spinner = dialogViewHolder.v(R.id.url_spinner);
+                        List<String> urls = new ArrayList<>();
+                        urls.add("选择服务器");
+                        if (urlList.isEmpty()) {
+                            urls.add(baseUrl);
+                        } else {
+                            urls.addAll(urlList);
+                        }
+
+                        spinner.setAdapter(new RSpinnerAdapter<String>(context, urls) {
+                            @Override
+                            public void onBindDropDownItemView(@NonNull RBaseViewHolder itemViewHolder, int position, String itemBean) {
+                                super.onBindDropDownItemView(itemViewHolder, position, itemBean);
+                                itemViewHolder.tv(R.id.base_text_view).setText(itemBean);
+                            }
+                        });
+
+                        final List<String> finalUrls = urls;
+                        spinner.setOnItemSelectedListener(new OnSpinnerItemSelected() {
+                            @Override
+                            public void onItemSelected(@Nullable AdapterView<?> parent, @Nullable View view, int position, long id) {
+                                super.onItemSelected(parent, view, position, id);
+                                if (position == 0) {
+                                    return;
+                                }
+                                String url = finalUrls.get(position);
+                                String[] split = url.split(" ");
+                                dialogViewHolder.exV(R.id.host_edit).setInputText(split[split.length - 1]);
                             }
                         });
 
